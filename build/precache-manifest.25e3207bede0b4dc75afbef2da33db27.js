9a8e29c68b6cfc567822d699eb22f41bef0b1d02
Suraj <kalagotlasuraj@gmail.com>
2023-07-17 16:47:49 +0530

    Going live

---
+++ build/precache-manifest.25e3207bede0b4dc75afbef2da33db27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7318dbce10100a673db403fb20d784e4",
+    "url": "/MicroMaze/index.html"
+  },
+  {
+    "revision": "15b9f0027823207132a7",
+    "url": "/MicroMaze/static/css/main.0832cd0d.chunk.css"
+  },
+  {
+    "revision": "3f51ea05862ffd4db38d",
+    "url": "/MicroMaze/static/js/2.f0583ba6.chunk.js"
+  },
+  {
+    "revision": "15b9f0027823207132a7",
+    "url": "/MicroMaze/static/js/main.3d0e77fc.chunk.js"
+  },
+  {
+    "revision": "9d5d4326b3dea25a315f",
+    "url": "/MicroMaze/static/js/runtime-main.4f77fecd.js"
+  }
+]);
